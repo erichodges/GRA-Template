2259eed0210252f0b57b201f0a2345d646ef4696
Eric Hodges <ehodges2004@gmail.com>
2017-12-12 14:19:46 -0800

    Added files

---
+++ src/index.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter } from 'react-router-dom'
+
+import { ApolloProvider } from 'react-apollo'
+import { ApolloClient } from 'apollo-client'
+import { HttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { GC_AUTH_TOKEN } from './constants'
+import { ApolloLink } from 'apollo-client-preset'
+
+const httpLink = new HttpLink({ uri: 'https://api.graph.cool/simple/v1/cjax3rfxo0prm01426vyoyplj' })
+
+const middlewareAuthLink = new ApolloLink((operation, forward) => {
+  const token = localStorage.getItem(GC_AUTH_TOKEN)
+  const authorizationHeader = token ? `Bearer ${token}` : null
+  operation.setContext({
+    headers: {
+      authorization: authorizationHeader
+    }
+  })
+  return forward(operation)
+})
+
+const httpLinkWithAuthToken = middlewareAuthLink.concat(httpLink)
+
+const client = new ApolloClient({
+  link: httpLinkWithAuthToken,
+  cache: new InMemoryCache()
+})
+
+
+ReactDOM.render(
+  <BrowserRouter>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </BrowserRouter>
+  , document.getElementById('root')
+)
+registerServiceWorker()
